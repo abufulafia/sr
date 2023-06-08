92b3b1fc9e1b0413feb7c11d9b5aa663b5dc909d
abufulafia <richard@rgrahn.net>
2023-06-08 17:13:09 +0200

    test committ

--- gen_SR_data_file_06-01-2023.R
+++ gen_SR_data_file_06-01-2023.R
@@ -1,7 +1,9 @@
 
 require(tidyverse)
+require(readxl)
 
-# produce file for strategic review 
+
+# produce file for strategic review data
 
 cols_needed <- c(1,57,65,73)
 
@@ -22,12 +24,10 @@ full_join(
   rename(iso3=1,
          hiv_lives_saved_proj_2017_2022_l=2,
          hiv_lives_saved_proj_2017_2022_m=3,
-         hiv_lives_saved_proj_2017_2022_u=4) %>% 
-  mutate(across(where(is.numeric),fns=as.double))
+         hiv_lives_saved_proj_2017_2022_u=4) 
   
   , 
     
-  
 # TB
   read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "TB KPI 1 e", skip = 4,range= "B5:BV120")  %>%
   select(cols_needed) %>% 
@@ -35,9 +35,8 @@ full_join(
          tb_lives_saved_proj_2017_2022_l=2,
          tb_lives_saved_proj_2017_2022_m=3,
          tb_lives_saved_proj_2017_2022_u=4) %>% 
-  mutate(across(where(is.numeric),fns=as.double))
+  mutate(across(c(2:4), as.double))  # converts character to numeric for later rounding
   
-  # mutate(tb_lives_saved_proj_2017_2022=as.double(tb_lives_saved_proj_2017_2022))
 ),  
 
    # malaria
@@ -46,14 +45,25 @@ full_join(
     rename(iso3=1,
            malaria_lives_saved_proj_2017_2022_l=2,
            malaria_lives_saved_proj_2017_2022_m=3,
-           malaria_lives_saved_proj_2017_2022_u=4) %>% 
-  mutate(across(where(is.numeric),fns=as.double))
-)
+           malaria_lives_saved_proj_2017_2022_u=4)  %>% 
+  mutate(across(c(2:4), as.double))
+  
+) 
   
 # ls_targets %>% mutate(across(where(is.numeric),fns=format(., digits=8, nsmall=2)))
 
+# ls_targets %>% mutate(across(where(is.numeric),fns=formatC(., format = "f", digitsls_targets %>% mutate(across(where(is.numeric),fns=formatC(., format = "f", digits=8, drop0trailing = FALSE)))
+
+ls_targets %>% mutate(across(where(is.double),fns=format(round((.),3),nsmall=3)))
+
+format(round(ls_targets$hiv_lives_saved_proj_2017_2022_l, 1), nsmall = 1)
 
 
+write_csv(ls_targets %>%
+            
+            mutate(across(where(is.double),fns=format(round((.),3),nsmall=3)))
+            
+          ,"ls_targets_rounded.csv")
 
 
 
@@ -96,60 +106,78 @@ write.csv(kpi_1a,"kpi_1a.csv", row.names = FALSE)
 
 # 2. GENERATE INCIDENCE REDUCTION DATA TARGET VS RESULTS ####
 
+
+# use na= Nan to replace NaN with blank and so get a numeric read in directly from readxl
 inc_rdn_targets <-
   # hiv
   full_join(    
     
     full_join(  
-      read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "HIV KPI 1 c", skip = 4, range= "B5:BV105")  %>%
-        select(1,65) %>% 
-        rename(iso3=1,hiv_inc_rdn_proj_2022_per=2) %>% 
-        mutate(hiv_inc_rdn_proj_2022_per=as.double(hiv_inc_rdn_proj_2022_per))
+      read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "HIV KPI 1 c", skip = 4, range= "B5:BV105", na="NaN")  %>%
+        select(cols_needed) %>% 
+        rename(iso3=1,
+               hiv_inc_rdn_proj_2022_per_l=2,
+               hiv_inc_rdn_proj_2022_per_m=3,
+               hiv_inc_rdn_proj_2022_per_u=4) 
       , 
       
       # TB
-      read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "TB KPI 1 c", skip = 4,range= "B5:BV120")  %>%
-        select(1,65) %>% 
-        rename(iso3=1,tb_inc_rdn_proj_2022_per=2) %>% 
-        mutate(tb_inc_rdn_proj_2022_per=as.double(tb_inc_rdn_proj_2022_per))
+      read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "TB KPI 1 c", skip = 4,range= "B5:BV120", na="NaN")  %>%
+        select(cols_needed) %>% 
+        rename(iso3=1,
+               tb_inc_rdn_proj_2022_per_l=2,
+               tb_inc_rdn_proj_2022_per_m=3,
+               tb_inc_rdn_proj_2022_per_u=4) 
+      # %>% 
+      # mutate(across(where(is.numeric),fns=as.double))
     ),  
     
     # malaria
-    read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "Malaria KPI 1 c", skip = 4,range= "B5:BV71")  %>%
-      select(1,65) %>% 
-      rename(iso3=1,malaria_inc_rdn_proj_2022_per=2) %>% 
-      mutate(malaria_inc_rdn_proj_2022_per=as.double(malaria_inc_rdn_proj_2022_per))
+    read_excel("data/allModelledKPIs_3Mar2017.xlsx", sheet = "Malaria KPI 1 c", skip = 4,range= "B5:BV71", na="NaN")  %>%
+      select(cols_needed) %>% 
+      rename(iso3=1,
+             mal_inc_rdn_proj_2022_per_l=2,
+             mal_inc_rdn_proj_2022_per_m=3,
+             mal_inc_rdn_proj_2022_per_u=4) 
+    # %>% 
+      # mutate(across(where(is.numeric),fns=as.double))
   ) %>% 
   filter(iso3!="GlobalFund")
 
+# # since the incidence reduction targets were framed with negative values as increases invert them
+# also divide them by 100 to express them as values between 0-1
+inc_rdn_targets <-
+inc_rdn_targets %>% 
+  mutate_at(vars(contains("inc_rdn_proj")), list(~. * -1)) %>% 
+  mutate_at(vars(contains("inc_rdn_proj")), list(~. /100)) 
 
-inc_rdn_actual <-
+
+# read in the incidence targets ####
+
+inc_rdn_results <-
 
   full_join(
     full_join(
-      # HIV
       read_excel("data/KPI1 gap analysis 2023_02_01_ph_SPH.xlsx", sheet = "summary",skip = 2, col_names = TRUE) %>% 
         select(Iso3,'% change in HIV incidence rate from 2015-2021') %>%
-        rename(hiv_inc_result_2015_2021_per= '% change in HIV incidence rate from 2015-2021'),
+        rename(hiv_inc_result_2015_2021_per= '% change in HIV incidence rate from 2015-2021'), 
       
       
-      # TB
       read_excel("data/KPI1 gap analysis 2023_02_01_ph_SPH.xlsx", sheet = "summary", skip = 2,col_names = TRUE) %>% 
-         select(Iso3,'% change in TB incidence rate from 2015 - 2021') %>%
-         rename(tb_inc_result_2015_2021_per= '% change in TB incidence rate from 2015 - 2021')
-      ),
+        select(Iso3,'% change in TB incidence rate from 2015 - 2021') %>%
+        rename(tb_inc_result_2015_2021_per= '% change in TB incidence rate from 2015 - 2021')
+    ),
     
-    # malaria
+    
     read_excel("data/KPI1 gap analysis 2023_02_01_ph_SPH.xlsx", sheet = "summary", skip = 2,col_names = TRUE) %>% 
       select(Iso3,'% change in Malaria incidence rate from 2015-2021') %>%
-      rename(malaria_inc_result_2015_2021_per= '% change in Malaria incidence rate from 2015-2021')
-  ) %>% 
+      rename(malaria_inc_result_2015_2021_per= '% change in Malaria incidence rate from 2015-2021')) %>% 
   rename(iso3=Iso3)%>% 
   filter(iso3!="GlobalFund")
 
 
 kpi_1b <- 
-  full_join(inc_rdn_targets,inc_rdn_actual)
+  full_join(inc_rdn_targets,inc_rdn_results)
 
 
 
@@ -157,19 +185,6 @@ kpi_1 <- full_join(kpi_1a,kpi_1b) %>%
   # replace NA substites zeros for both NA and NaN so this deals with the NaNs
   mutate(across(everything(), ~replace_na(.x, NA)))
 
-kpi_1 <- 
-  # filter out cases where all the following are missing | functions as AND
-kpi_1 %>% 
-               
-
-
-# since the incidence reducton targets were framed with negative values as increases invert them
-kpi_1 <-
-  kpi_1 %>% 
-  mutate_at(vars(contains("inc_rdn_proj")), list(~. * -1))
-
-
-# write.csv(kpi_1,"kpi_1.csv", row.names = FALSE)
 
 
 wb <- openxlsx::createWorkbook()
@@ -177,9 +192,15 @@ openxlsx::addWorksheet(wb,"KPI1a_analysis")
 openxlsx::writeDataTable(wb,"KPI1a_analysis", 
                          kpi_1 %>% 
                            select(iso3,contains("lives_saved")) %>% 
-                           filter(!is.na(hiv_lives_saved_proj_2017_2022) 
-                                  | !is.na(tb_lives_saved_proj_2017_2022)
-                                  | !is.na(malaria_lives_saved_proj_2017_2022) 
+                           filter(!is.na(hiv_lives_saved_proj_2017_2022_l)
+                                  |!is.na(hiv_lives_saved_proj_2017_2022_m)
+                                  |!is.na(hiv_lives_saved_proj_2017_2022_u)
+                                  | !is.na(tb_lives_saved_proj_2017_2022_l)
+                                  | !is.na(tb_lives_saved_proj_2017_2022_m)
+                                  | !is.na(tb_lives_saved_proj_2017_2022_u)
+                                  | !is.na(malaria_lives_saved_proj_2017_2022_l)
+                                  | !is.na(malaria_lives_saved_proj_2017_2022_m)
+                                  | !is.na(malaria_lives_saved_proj_2017_2022_u)
                                   | !is.na(hiv_lives_saved_result_2017_2021) 
                                   | !is.na(tb_lives_saved_result_2017_2021) 
                                   | !is.na(malaria_lives_saved_result_2017_2021))
@@ -188,15 +209,21 @@ openxlsx::writeDataTable(wb,"KPI1a_analysis",
 openxlsx::addWorksheet(wb,"KPI1b_analysis")
 openxlsx::writeDataTable(wb,"KPI1b_analysis", kpi_1 %>%  
                            select(iso3,contains("inc")) %>% 
-                         filter(!is.na(hiv_inc_rdn_proj_2022_per)
-                                |!is.na(tb_inc_rdn_proj_2022_per) 
-                           | !is.na(malaria_inc_rdn_proj_2022_per) 
+                         filter(!is.na(hiv_inc_rdn_proj_2022_per_l)
+                                |!is.na(hiv_inc_rdn_proj_2022_per_m)
+                                |!is.na(hiv_inc_rdn_proj_2022_per_u)
+                                |!is.na(tb_inc_rdn_proj_2022_per_l) 
+                                |!is.na(tb_inc_rdn_proj_2022_per_m)
+                                |!is.na(tb_inc_rdn_proj_2022_per_u) 
+                           | !is.na(mal_inc_rdn_proj_2022_per_l)
+                           | !is.na(mal_inc_rdn_proj_2022_per_m)
+                           | !is.na(mal_inc_rdn_proj_2022_per_u)
                            | !is.na(hiv_inc_result_2015_2021_per) 
                            | !is.na(tb_inc_result_2015_2021_per)
                            | !is.na(malaria_inc_result_2015_2021_per))
                          , startCol = 1, startRow = 1)
 
-openxlsx::saveWorkbook(wb,paste0("KPI1_SR_data",Sys.Date(),".xlsx"), overwrite = TRUE)
+openxlsx::saveWorkbook(wb,paste0("KPI1_SR_data_",Sys.Date(),".xlsx"), overwrite = TRUE)
 
 
 
